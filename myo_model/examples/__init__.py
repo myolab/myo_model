24812362d4100832cf23306640c5ab4c238182b8
Vittorio-Caggiano <caggiano@gmail.com>
2026-01-15 15:53:52 -0500

    update to myo_model v2

--- myo_model/examples/__init__.py
+++ myo_model/examples/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) $(date +%Y) MyoLab, Inc.
+Copyright (c) 2026 MyoLab, Inc.
 Released under the MyoLab Non-Commercial Scientific Research License
 on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 either express or implied.
@@ -8,6 +8,4 @@ You may not use this file except in compliance with the License.
 See the LICENSE file for governing permissions and limitations.
 """
 
-
 # Summary: Example script: __init__
-
--- myo_model/examples/load_model_with_markers.py
+++ myo_model/examples/load_model_with_markers.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) $(date +%Y) MyoLab, Inc.
+Copyright (c) 2026 MyoLab, Inc.
 Released under the MyoLab Non-Commercial Scientific Research License
 on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 either express or implied.
@@ -7,7 +7,6 @@ either express or implied.
 You may not use this file except in compliance with the License.
 See the LICENSE file for governing permissions and limitations.
 """
-
 
 # Summary: Example script: load_model_with_markers
 
